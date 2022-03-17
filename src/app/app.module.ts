f96f485b834b037cf3774b2fb605043803ece2c9
Kienn Thiett <smoothkt4951@gmail.com>
2022-03-17 15:48:10 +0700

    initial + add code + Compiled successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MsalGuard, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: 'e5e627c0-ee32-427b-8cc5-1841b7fefbb9',
+        authority: '0745e4e9-2462-4b7a-97e2-4051a4ce1aa0',
+        redirectUri: 'http://localhost:4200'
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+      }
+    }), {
+      interactionType: InteractionType.Redirect, // MSAL Guard Configuration
+      authRequest: {
+        scopes: ['user.read']
+      }
+  }, {
+    interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
+    protectedResourceMap: new Map([ 
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+    ])
+  })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MsalGuard],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
